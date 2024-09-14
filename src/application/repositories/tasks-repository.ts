e209ea54a00a9d592d9c969148d23d7767a36082
Rafinhaa <rafinhaa@me.com>
2024-09-14 16:13:43 -0300

    feat: task repositories

---
+++ src/application/repositories/tasks-repository.ts
@@ -0,0 +1,10 @@
+import { Task } from "@/domain/entities/task";
+import { User } from "@/domain/entities/user";
+
+
+export interface TasksRepository {
+  create(task: Task): Promise<void>;
+  findManyByUserId(user: User ): Promise<Task[] | null>;
+  deleteTask(task: Task): Promise<void>;
+  completeTask(task: Task): Promise<void>;
+}
