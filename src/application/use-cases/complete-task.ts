f98b076f1be118e2894b25352a716c1061689953
Rafinhaa <rafinhaa@me.com>
2024-09-14 17:46:41 -0300

    feat: complete task use case

---
+++ src/application/use-cases/complete-task.ts
@@ -0,0 +1,46 @@
+import { UsersRepository } from "@/application/repositories/users-repository"; 
+import { Either, left, right } from "@/core/either";
+import { Task, TaskProps } from "@/domain/entities/task";
+import { NotFoundError } from "./errors/not-found-error";
+import { TasksRepository } from "../repositories/tasks-repository";
+import { UniqueEntityId } from "@/core/entities/unique-entity-id";
+import { NotPermissionError } from "./errors/not-permission-error";
+import { TaskStatus } from "@/domain/value-objects/task-status";
+
+interface CompleteTaskUseCaseRequest {
+  taskId: string
+  userId: string
+}
+
+type CompleteTaskUseCaseResponse = Either<NotFoundError | NotPermissionError, { task: Task }>;
+
+export class CompleteTaskUseCase {
+
+	constructor(
+    private tasksRepository: TasksRepository, 
+    private userRepository: UsersRepository) {}
+
+  async execute({ taskId, userId}: CompleteTaskUseCaseRequest) : Promise<CompleteTaskUseCaseResponse> {
+    
+		const user = await this.userRepository.findById({ id: new UniqueEntityId(userId) });
+
+    if(!user)
+      return left(new NotFoundError("User not found"));
+    
+    const task = await this.tasksRepository.findById({ id: new UniqueEntityId(taskId) });		
+
+    if(!task)
+      return left(new NotFoundError("Task not found"));
+    
+    if (task.userId.toString() !== userId) 
+      return left(new NotPermissionError("User does not own this task"));
+
+    task.status = TaskStatus.DONE
+
+    await this.tasksRepository.completeTask(task);
+
+    return right({
+      task
+    });
+  }
+}
