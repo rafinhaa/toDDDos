0547d8a9b574c5ab6aaf8cc8b22fb922b5f565e6
Rafinhaa <rafinhaa@me.com>
2024-09-14 14:11:05 -0300

    feat: core entities

---
+++ src/core/entities/unique-entity-id.ts
@@ -0,0 +1,17 @@
+import { randomUUID } from "node:crypto";
+
+export class UniqueEntityId {
+  private value: string;
+
+  toString() {
+    return this.value;
+  }
+
+  constructor(value?: string) {
+    this.value = value ?? randomUUID();
+  }
+
+  public equals(id: UniqueEntityId) {
+    return id.toString() === this.value;
+  }
+}
