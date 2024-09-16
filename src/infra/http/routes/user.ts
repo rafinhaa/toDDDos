3a48e9dfe6cdfc05f7bf12252bb45f7484980b88
Rafinhaa <rafinhaa@me.com>
2024-09-16 18:36:03 -0300

    feat: use register route

---
+++ src/infra/http/routes/user.ts
@@ -0,0 +1,47 @@
+import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod"
+import z from "zod"
+
+import { DrizzleUsersRepository } from "@/infra/database/drizzle/repositories/drizzle-users-repository"
+import { userRegisterController } from "@/infra/http/controllers/user"
+
+const bodySchema = z
+  .object({
+    email: z.string().email(),
+    password: z.string().min(6),
+    confirmPassword: z.string().min(6),
+  })
+  .superRefine(({ confirmPassword, password }, ctx) => {
+    if (confirmPassword !== password) {
+      ctx.addIssue({
+        code: "custom",
+        message: "The passwords did not match",
+        path: ["confirmPassword"],
+      })
+    }
+  })
+
+const schema = {
+  body: bodySchema,
+}
+
+export const users: FastifyPluginAsyncZod = async (app) => {
+  app.post(
+    "/",
+    {
+      schema,
+    },
+    async (req, rep) => {
+      const { email, password } = req.body
+
+      const result = await userRegisterController({
+        usersRepository: new DrizzleUsersRepository(),
+        email,
+        password,
+      })
+
+      if (result.isLeft()) throw result.value
+
+      return rep.status(201)
+    },
+  )
+}
