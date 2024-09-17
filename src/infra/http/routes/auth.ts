902e0ee5e39a098d818d9550abb40077e2246d5a
Rafinhaa <rafinhaa@me.com>
2024-09-17 11:30:36 -0300

    refactor: auth routes

--- src/infra/http/routes/auth.ts
+++ src/infra/http/routes/auth.ts
@@ -2,24 +2,40 @@ import type { FastifyPluginAsyncZod } from "fastify-type-provider-zod"
 import z from "zod"
 
 import { AuthUserUseCase } from "@/application/use-cases/auth-user"
+import { CreateUserUseCase } from "@/application/use-cases/create-users"
 import { DatabaseUsersRepository } from "@/infra/database"
 import { HashComparer } from "@/infra/encryption"
+import { HashGenerator } from "@/infra/encryption"
 import { UserPresenter } from "@/infra/http/presenters/user-presenter"
 
-const bodySchema = z.object({
+const signInBodySchema = z.object({
   email: z.string().email(),
   password: z.string().min(6),
 })
 
-const schema = {
-  body: bodySchema,
-}
+const registerBodySchema = z
+  .object({
+    email: z.string().email(),
+    password: z.string().min(6),
+    confirmPassword: z.string().min(6),
+  })
+  .superRefine(({ confirmPassword, password }, ctx) => {
+    if (confirmPassword !== password) {
+      ctx.addIssue({
+        code: "custom",
+        message: "The passwords did not match",
+        path: ["confirmPassword"],
+      })
+    }
+  })
 
 export const auth: FastifyPluginAsyncZod = async (app) => {
   app.post(
-    "/",
+    "/sign-in",
     {
-      schema,
+      schema: {
+        body: signInBodySchema,
+      },
     },
     async (req, rep) => {
       const { email, password } = req.body
@@ -39,6 +55,30 @@ export const auth: FastifyPluginAsyncZod = async (app) => {
       const token = app.jwt.sign({ id: user.id })
 
       return rep.status(200).send({ token })
+    },
+  )
+
+  app.post(
+    "/register",
+    {
+      schema: {
+        body: registerBodySchema,
+      },
+    },
+    async (req, rep) => {
+      const { email, password } = req.body
+
+      const result = await new CreateUserUseCase(
+        new DatabaseUsersRepository(),
+        new HashGenerator(),
+      ).execute({
+        email,
+        password,
+      })
+
+      if (result.isLeft()) throw result.value
+
+      return rep.status(201)
     },
   )
 }
